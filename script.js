99a69c2f1a09efa53109f1dfc03a5bc25860bc7a
sr5392 <61883265+sr5392@users.noreply.github.com>
2023-07-15 16:07:11 +0200

    Add slider for grid size

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-const gridContainer = document.createElement("div");
-gridContainer.classList.add("grid-container");
+const slider = document.querySelector("#slider");
+slider.addEventListener("input", updateGrid);
+
+const pGridSize = document.querySelector("#grid-size");
+const gridContainer = document.querySelector("#grid-container");
 
 const gridItem = document.createElement("div");
 gridItem.classList.add("grid-item");
 
-for (let i = 0; i < 256; i++) {
-    gridContainer.appendChild(gridItem.cloneNode());
+function updateGrid() {
+    gridContainer.innerHTML = "";
+
+    const gridSize = slider.value;
+    const gridItemSize = 64 / gridSize;
+
+    gridItem.style.height = gridItemSize + "vmin";
+    gridItem.style.width = gridItemSize + "vmin";
+   
+    for(let i = 0; i < gridSize**2; i++) {
+        gridContainer.appendChild(gridItem.cloneNode());
+    }
 }
 
-document.body.appendChild(gridContainer);
+updateGrid();
+
