0929ff241e78afa0855bc8c6443d0ffb691b5699
sr5392 <61883265+sr5392@users.noreply.github.com>
2023-07-15 18:02:24 +0200

    Add drawing functionality

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let mouseHold = false;
+document.addEventListener("mousedown", mouseAction);
+document.addEventListener("mouseup", mouseAction);
+
 const slider = document.querySelector("#slider");
 slider.addEventListener("input", updateGrid);
 
@@ -16,9 +20,41 @@ function updateGrid() {
     gridItem.style.height = gridItemSize + "vmin";
     gridItem.style.width = gridItemSize + "vmin";
    
-    for(let i = 0; i < gridSize**2; i++) {
-        gridContainer.appendChild(gridItem.cloneNode());
+    for (let i = 0; i < gridSize**2; i++) {
+        let tmpGridItem = gridItem.cloneNode();
+        tmpGridItem.addEventListener("mouseover", updateGridItem);
+        tmpGridItem.addEventListener("mousedown", updateGridItem)
+        tmpGridItem.addEventListener("dragstart", preventDragging);
+
+        gridContainer.appendChild(tmpGridItem);
     }
+
+    pGridSize.innerText = gridSize;
+}
+
+function mouseAction(event) {
+    if (event.type == "mousedown") {
+        mouseHold = true;
+        return;
+    }
+
+    mouseHold = false;
+}
+
+function updateGridItem(event) {
+    if (event.type == "mouseover") {
+        if (mouseHold) {
+            this.style.backgroundColor = "black";
+        }
+
+        return;
+    }
+    
+    this.style.backgroundColor = "black";     
+}
+
+function preventDragging(event) {
+    event.preventDefault();
 }
 
 updateGrid();
